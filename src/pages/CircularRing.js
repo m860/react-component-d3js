16a7d10d266ceea20aa9048019bd3ec99b37d702
jean <jean.ma.1986@gmail.com>
2017-10-09 15:49:31 +0800

    fix

--- src/pages/CircularRing.js
+++ src/pages/CircularRing.js
@@ -6,57 +6,68 @@ import BasePage from './BasePage'
 import Layout from '../components/LayoutWithNavigation'
 import * as d3 from 'd3'
 
-export default class CircularRing extends BasePage{
-	render(){
+export default class CircularRing extends BasePage {
+	render() {
 		return (
 			<Layout>
 				<svg id="chart"></svg>
 			</Layout>
 		);
 	}
-	componentDidMount(){
+
+	componentDidMount() {
 		super.componentDidMount();
-		const chart=d3.select('#chart')
-			.attr('width',800)
-			.attr("height",600)
-			.attr("style","background-color:silver;fill:gray;stroke:black");
-		const center={
-			x:400,
-			y:300
+		const chart = d3.select('#chart')
+			.attr('width', 800)
+			.attr("height", 600)
+			.attr("style", "background-color:silver;fill:gray;stroke:black");
+		const center = {
+			x: 400,
+			y: 300
 		};
 
-		const r=100;
-		const circleLength=Math.PI*r*2;
-		const width=20;
+		const r = 100;
+		const circleLength = Math.PI * r * 2;
+		const width = 20;
 
-		let circle1=chart.append('circle')
-			.attr('cx',center.x)
-			.attr('cy',center.y)
-			.attr('fill','transparent')
-			.attr('stroke-dasharray',circleLength)
-			.attr('stroke-dashoffset',0)
-			.attr('stroke-width',width)
-			.attr('r',r);
+		chart.append('circle')
+			.attr('cx', 150)
+			.attr('cy', 150)
+			.attr('r', 100)
+			.attr('stroke-dasharray', '10,5')
+			.attr('stroke-dashoffset', 0)
+			.attr('stroke-width', 20)
+			.attr('stroke', 'green')
+			.attr('fill','none')
 
-		let circle2=chart.append('circle')
-			.attr('cx',center.x)
-			.attr('cy',center.y)
-			.attr('fill','transparent')
-			.attr('stroke-dasharray',circleLength)
-			.attr('stroke-dashoffset',0)
-			.attr('stroke-width',width)
-			.attr('stroke','red')
-			.attr('style','transition:all 0.51s')
-			.attr('r',r);
-		const updatePercent=(percent=1)=>{
-			if(percent>1){
-				percent=0;
+		let circle1 = chart.append('circle')
+			.attr('cx', center.x)
+			.attr('cy', center.y)
+			.attr('fill', 'transparent')
+			.attr('stroke-dasharray', circleLength)
+			.attr('stroke-dashoffset', 0)
+			.attr('stroke-width', width)
+			.attr('r', r);
+
+		let circle2 = chart.append('circle')
+			.attr('cx', center.x)
+			.attr('cy', center.y)
+			.attr('fill', 'transparent')
+			.attr('stroke-dasharray', circleLength)
+			.attr('stroke-dashoffset', 0)
+			.attr('stroke-width', width)
+			.attr('stroke', 'red')
+			.attr('style', 'transition:all 0.51s')
+			.attr('r', r);
+		const updatePercent = (percent = 1)=> {
+			if (percent > 1) {
+				percent = 0;
 			}
-			circle2.attr('stroke-dashoffset',circleLength*(1-percent));
-			percent+=0.01;
-			setTimeout(()=>{
+			circle2.attr('stroke-dashoffset', circleLength * (1 - percent));
+			percent += 0.01;
+			setTimeout(()=> {
 				updatePercent(percent)
-			},100);
+			}, 100);
 		}
 		updatePercent();
 	}
